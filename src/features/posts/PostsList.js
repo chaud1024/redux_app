0e25e9fddbb795cf46d7be39ecd158ebc82fe33b
chaud1024 <chaud1024@gmail.com>
2023-02-09 16:46:28 +0900

    add Component PostList

---
+++ src/features/posts/PostsList.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useSelector } from "react-redux";
+import { selectAllPosts } from './postsSlice';
+
+const PostsList = () => {
+    
+    // state구조가 바뀔 경우를 대비하자
+    // useSelector(state => state.posts)에서 state.posts.post.comment 등등 내부 구조가 복잡해진다면
+    // selector를 slice안에서 만들고 export하는 게 좋음
+    const posts = useSelector(selectAllPosts)
+    // 이렇게 함으로써 state의 구조가 바뀌더라도 그 변경사항을 slice에서 바꿔주기만하면 됨,
+    //모든 컴포넌트에 들어가서 변경하지 않아도 됨
+
+    const renderedPosts = posts.map((post) => (
+        <article key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content.substring(0, 100)}</p>
+        </article>
+    ))
+
+  return (
+    <section>
+        <h2>Posts</h2>
+        {renderedPosts}
+    </section>
+  )
+}
+
+export default PostsList
