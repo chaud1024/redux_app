d6d8ebd3ab89fb838c3f84b922d19dd72d8ff450
chaud1024 <chaud1024@gmail.com>
2023-02-09 21:08:50 +0900

    refactor code

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = [
     {
@@ -17,12 +17,20 @@ const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
-        // reducers 안에 함수를 만들면, createSlice는 자동적으로 action creator 함수를 같은 이름으로 만들어낸다
-        // 그래서 우리가 reducers 안의 action들을 export하면 사실 자동생성된 action creator function이 export되는 것이다.
-        postAdded(state, action) {
-            // createSlice안에서만 작동하기에 state가 mutate되지 않음
-            // 직접 state를 set할 수 있음
-            state.push(action.payload)
+        postAdded: {
+            reducer (state, action) {
+                state.push(action.payload)
+            },
+            prepare(title, content, userId) {
+                return {
+                    payload: {
+                        id: nanoid(),
+                        title,
+                        content,
+                        userId
+                    }
+                }
+            }
         }
     }
 })
