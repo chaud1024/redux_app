5de2bd62b5808783dc79e672c788f978431e0942
chaud1024 <chaud1024@gmail.com>
2023-02-09 21:44:27 +0900

    edit post.user -> post.userId

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -17,7 +17,7 @@ const PostsList = () => {
             <h3>{post.title}</h3>
             <p>{post.content.substring(0, 100)}</p>
             <p className='postCredit'>
-              <PostAuthor userId={post.user} />
+              <PostAuthor userId={post.userId} />
             </p>
         </article>
     ))
