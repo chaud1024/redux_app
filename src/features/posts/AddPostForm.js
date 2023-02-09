a447ff22e2a7167eb5843ce3383e4d0e2840be4d
chaud1024 <chaud1024@gmail.com>
2023-02-09 17:35:26 +0900

    add basic form of add post

---
+++ src/features/posts/AddPostForm.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+
+const AddPostForm = () => {
+
+    // 전역이 아니라, 이 컴포넌트에서만 쓰는 것이기 때문에 useState사용
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+
+    const onTitleChanged = e => setTitle(e.target.value)
+    const onContentChanged = e => setContent(e.target.value)
+
+  return (
+    <section>
+            <h2>Add a New Post</h2>
+            <form>
+                <label htmlFor="postTitle">Post Title:</label>
+                <input
+                    type="text"
+                    id="postTitle"
+                    name="postTitle"
+                    value={title}
+                    onChange={onTitleChanged}
+                />
+
+                <label htmlFor="postContent">Content:</label>
+                <textarea
+                    id="postContent"
+                    name="postContent"
+                    value={content}
+                    onChange={onContentChanged}
+                />
+            </form>
+        </section>
+  )
+}
+
+export default AddPostForm
