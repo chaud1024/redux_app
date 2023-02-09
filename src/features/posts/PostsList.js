8e3f00e90a56032b0666bfaa5320a4c940876f55
chaud1024 <chaud1024@gmail.com>
2023-02-09 21:09:55 +0900

    import post author component into postsList

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from "react-redux";
 import { selectAllPosts } from './postsSlice';
+import PostAuthor from './PostAuthor';
 
 const PostsList = () => {
     
@@ -15,6 +16,9 @@ const PostsList = () => {
         <article key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.content.substring(0, 100)}</p>
+            <p className='postCredit'>
+              <PostAuthor userId={post.user} />
+            </p>
         </article>
     ))
 
