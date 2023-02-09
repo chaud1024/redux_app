4fd03c814b72bf05d30cc521af35b09db52cab0b
chaud1024 <chaud1024@gmail.com>
2023-02-09 17:41:52 +0900

    add AddPostForm

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,7 +1,11 @@
 import { useState } from "react";
-
+import { useDispatch } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
+// nanoid 는 random id 생성을 도와줍니다
+import { postAdded } from "./postsSlice";
 
 const AddPostForm = () => {
+    const dispatch = useDispatch()
 
     // 전역이 아니라, 이 컴포넌트에서만 쓰는 것이기 때문에 useState사용
     const [title, setTitle] = useState('')
@@ -10,28 +14,49 @@ const AddPostForm = () => {
     const onTitleChanged = e => setTitle(e.target.value)
     const onContentChanged = e => setContent(e.target.value)
 
+    const onSavePostClidked = () => {
+        if(title && content) {
+            dispatch(
+                postAdded({
+                    id: nanoid(),
+                    title,
+                    content
+                })
+            )
+
+            setTitle('')
+            setContent('')
+        }
+    }
+
   return (
     <section>
-            <h2>Add a New Post</h2>
-            <form>
-                <label htmlFor="postTitle">Post Title:</label>
-                <input
-                    type="text"
-                    id="postTitle"
-                    name="postTitle"
-                    value={title}
-                    onChange={onTitleChanged}
-                />
+        <h2>Add a New Post</h2>
+        <form>
+            <label htmlFor="postTitle">Post Title:</label>
+            <input
+                type="text"
+                id="postTitle"
+                name="postTitle"
+                value={title}
+                onChange={onTitleChanged}
+            />
 
-                <label htmlFor="postContent">Content:</label>
-                <textarea
-                    id="postContent"
-                    name="postContent"
-                    value={content}
-                    onChange={onContentChanged}
-                />
-            </form>
-        </section>
+            <label htmlFor="postContent">Content:</label>
+            <textarea
+                id="postContent"
+                name="postContent"
+                value={content}
+                onChange={onContentChanged}
+            />
+        </form>
+        <button
+            type="button"
+            onClick={onSavePostClidked}
+        >
+            Save Post
+        </button>
+    </section>
   )
 }
 
