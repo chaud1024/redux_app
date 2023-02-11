bc15ac51988ec0d4f01f06cd601baef9c641f386
chaud1024 <chaud1024@gmail.com>
2023-02-11 20:10:09 +0900

    add Route

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,7 +2,7 @@ import AddPostForm from "./features/posts/AddPostForm";
 import PostsList from "./features/posts/PostsList";
 import SinglePostPage from "./features/posts/SinglePostPage";
 import Layout from "./components/Layout";
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 import EditPostForm from "./features/posts/EditPostForm";
 import UsersList from "./features/users/UsersList";
 import UserPage from "./features/users/UserPage";
@@ -26,6 +26,12 @@ function App() {
           <Route index element={<UsersList />} />
           <Route path=":userId" element={<UserPage />} />
         </Route>
+
+        {/* Catch all - replace with 404 component if you want */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+        {/* *: going to select any route that makes it past these other routes */}
+        {/* if it doesn't match any of these, it will go to home */}
+        {/* replace: will replace the bad request */}
 
       </Route>
     </Routes>
