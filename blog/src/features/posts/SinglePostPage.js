48c218a2c128f3425328fe0963f37e31204b2965
chaud1024 <chaud1024@gmail.com>
2023-02-11 15:25:31 +0900

    add Links

--- blog/src/features/posts/SinglePostPage.js
+++ blog/src/features/posts/SinglePostPage.js
@@ -6,7 +6,7 @@ import TimeAgo from './TimeAgo';
 import ReactionButton from './ReactionButton';
 
 import { useParams } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 
 const SinglePostPage = () => {
 
@@ -27,6 +27,7 @@ const SinglePostPage = () => {
         <h3>{post.title}</h3>
         <p>{post.body}</p>
         <p className='postCredit'>
+            <Link to={`/post/edit/${post.id}`}>Edit Post</Link>
             <PostAuthor userId={post.userId} />
             <TimeAgo timestamp={post.date}/>
         </p>
